0f514f962dd3a65db602ef76f7c14c51dd8cb58d
Adam Hosticka <adamhosticka@gmail.com>
2019-01-19 00:36:36 +0100

    authentication 02

--- server.js
+++ server.js
@@ -65,12 +65,6 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(express.static(path.join(__dirname, 'public')));
-app.use(session({
-            secret: 'keyboard cat',
-            resave: false,
-            saveUninitialized: true,
-            cookie: { maxAge: 60000 }
-        }))
 
         
 app.get('/', user.home);//call for main index page
