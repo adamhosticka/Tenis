b425377dfc0e0233155d3eee7edfad4e258f950e
Adam Hosticka <adamhosticka@gmail.com>
2019-01-28 16:28:39 +0100

    auth stucked

--- server.js
+++ server.js
@@ -131,32 +131,32 @@ app.get('/load_colors', function(req, res) {
 
 app.post('/login', function(req, res) {
     var message = '';
-  var sess = req.session; 
+    var sess = req.session; 
 
-  if(req.method == "POST"){
-     var post  = req.body;
-     var name= post.user_name;
-     var pass= post.password;
-    
-     var sql="SELECT id, first_name, last_name, user_name FROM `users` WHERE `user_name`='"+name+"' and password = '"+pass+"'";     
-     console.log(sql)                      
-     db.query(sql, function(err, results){  
-       console.log(results)    
-        if(results.length){
-           req.session.userId = results[0].id;
-           req.session.user = results[0];
-           console.log(results[0].id);
-           res.redirect('/index_admin');
-        }
-        else{
-           message = 'Neplatné údaje.';
-           res.render('index.ejs',{message: message});
-        }
-                
-     });
-  } else {
-     res.render('index.ejs',{message: message});
-  }
+    if(req.method == "POST"){
+        var post  = req.body;
+        var name= post.user_name;
+        var pass= post.password;
+        
+        var sql="SELECT id, first_name, last_name, user_name FROM `users` WHERE `user_name`='"+name+"' and password = '"+pass+"'";     
+        console.log(sql)                      
+        con.query(sql, function(err, results){  
+        console.log(results)    
+            if(results.length){
+            req.session.userId = results[0].id;
+            req.session.user = results[0];
+            console.log(results[0].id);
+            res.redirect('/index_admin');
+            }
+            else{
+            message = 'Neplatné údaje.';
+            res.render('index.ejs',{message: message});
+            }
+                    
+        });
+    } else {
+        res.render('index.ejs',{message: message});
+    }
       
 })
 
